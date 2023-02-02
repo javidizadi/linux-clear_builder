d67c66cbef97d222673ecf2e0cc4881c5ed2d381
Javid izadi <javid.izadi1384@gmail.com>
2023-02-02 19:24:33 +0330

    [fix] prev [feat]

--- Dockerfile
+++ Dockerfile
@@ -2,6 +2,7 @@ FROM archlinux:latest
 RUN pacman -Suy --noconfirm --needed git base-devel bc cpio libelf pahole xmlto github-cli
 RUN useradd -m linux-clear_builder
 USER linux-clear_builder
-COPY modprobed.db /modprobed.db
+RUN mkdir .config
+COPY modprobed.db /home/linux-clear_builder/.config/modprobed.db
 COPY entrypoint.sh /entrypoint.sh
 ENTRYPOINT [ "/entrypoint.sh" ]
--- entrypoint.sh
+++ entrypoint.sh
@@ -7,7 +7,7 @@ cd linux-clear
 echo "Checkout specified commit..."
 git checkout $commit_hash &&
 echo "Compiling kernel..."
-env MAKEFLAGS="-s -j$(nproc) LSMOD=/modprobed.db localmodconfig" _subarch=22 makepkg --skippgpcheck &&
+env MAKEFLAGS="-s -j$(nproc)" _localmodcfg=y _subarch=22 makepkg --skippgpcheck &&
 echo "Logining in to GitHub..."
 printenv GITHUB_KEY | gh auth login --with-token
 version=`git log --format=%B -n 1 $commit_hash | awk -F '-' 'NR==1{print "v"$1}'`
