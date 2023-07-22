dc9d05a62cc3d397bc229660d7360641211136a9
Javid izadi <javid.izadi1384@gmail.com>
2023-07-22 20:55:44 +0330

    [test]

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,23 +1,23 @@
 #!/bin/bash
+set -e
 export PATH="/usr/lib/ccache/bin:$PATH"
 commit_hash="caa5df2fb6a817f85c9ec94c5af8d2d253a8fecd"
 repo=`printenv REPO`
 cd /home/builder
-git clone https://aur.archlinux.org/linux-clear.git &&
+git clone https://aur.archlinux.org/linux-clear.git
 cd linux-clear
 echo "Checkout specified commit..."
-git checkout $commit_hash &&
-# ls -ltrha /home/builder/.cache/
-# exit 0
-chown -R builder $HOME/.cache/ccache
+git checkout $commit_hash
 echo "Compiling kernel..."
-env MAKEFLAGS="-s -j$(nproc)" _localmodcfg=y _subarch=22 makepkg --skippgpcheck &&
+env MAKEFLAGS="-s -j$(nproc)" _localmodcfg=y _subarch=22 makepkg --skippgpcheck
 cache_size=$(du -sh $HOME/.cache/ccache)
 echo "Your cache size: ${cache_size}"
 echo "Logining in to GitHub..."
 printenv GITHUB_KEY | gh auth login --with-token
 version=`git log --format=%B -n 1 $commit_hash | awk -F '-' 'NR==1{print "v"$1}'`
+set +e
 gh release view "$version" --repo "$repo"
+set -e
 tag_exists=$?
 if test $tag_exists -eq 0; then
     echo "Tag already exists!"
@@ -27,7 +27,7 @@ else
     echo "Tag does not exist!"
 fi
 echo "Releasing $version binaries into $repo"
-gh release create "$version" ./*.pkg.tar.zst --repo "$repo" &&
+gh release create "$version" ./*.pkg.tar.zst --repo "$repo"
 echo "Released!"
 echo "Loging out from Github..."
 gh auth logout -h github.com
